c32534b84c7524a235e8e75d4e9968cd89682615
AamirMalikMi6 <m.aamir.malik.mi6@gmail.com>
2024-07-01 16:30:37 +0500

    c

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Install Composer and dependencies
-composer install --optimize-autoloader --no-dev
-
-# Run Laravel commands
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
